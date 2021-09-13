6dbcb3c7426b93dbe7211b8b1f34d34f6288b075
singram11 <ingramsarah11@gmail.com>
2021-09-13 14:31:48 -0700

    Delete extra files, add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import PlayButton from './PlayButton'
 
 function App() {
+
+  const [isPlay, setIsPlay] = useState(false)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       Silly Amy Game!
       </header>
+      <PlayButton setIsPlay={setIsPlay}/>
     </div>
   );
 }
