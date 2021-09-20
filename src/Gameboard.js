55ff23559c916fa73b756f18af2fa9b876db3d72
singram11 <ingramsarah11@gmail.com>
2021-09-20 14:56:25 -0700

    Randomize cards

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -6,20 +6,33 @@ import Card from "./Card"
 
 function Gameboard(props){
 
-        const cardArr = []
-        for (const image in images) {
-          
-            const card = <Card
-            key={image}
-            className={images[image]["imgName"]}
-            imgSrc = {images[image]["img"]}/>
-            
-            cardArr.push(card)
-        }
-    
-        console.log(cardArr)
+    const [ numberFlipped, setNumberFlipped ] = useState(0)
+    const [ card1, setCard1 ] = useState(null)
+    const [ card2, setCard2 ] = useState(null)
 
-    return <Container><Row><div>{cardArr}</div></Row></Container>
+    let cardArr = []
+    for (const image in images) {
+        
+        const card = <Card
+        key={image}
+        className={images[image]["imgName"]}
+        imgSrc = {images[image]["img"]}/>
+        
+        cardArr.push(card)
+    }
+    //shuffle arrray
+    let shuffledCardArr = cardArr
+        .map((value) => ({value, sort: Math.random() }))
+        .sort((a,b) => a.sort - b.sort)
+        .map(({value}) => value)
+
+    function checkMatch(numberFlipped, card1, card2) {
+
+        
+    }
+    
+
+    return <Container><Row><div>{shuffledCardArr}</div></Row></Container>
 }
 
 export default Gameboard;
