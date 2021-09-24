2dea9ca175a8e1d5027dc776b9b738813897ef9f
singram11 <ingramsarah11@gmail.com>
2021-09-23 20:46:06 -0700

    Finish matching function

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -8,7 +8,7 @@ function Gameboard(props){
 
    const [cardsFaceUp, setCardsFaceUp] = useState([])
    const [cardsMatched, setCardsMatched] = useState([])
-   const [cardDeck, setCardDeck] = useState()
+
 
     console.log("type of cardsFaceUp:")
    console.log(typeof(cardsFaceUp))
@@ -18,11 +18,10 @@ function Gameboard(props){
    
    
    let cardArr = images.map((image, index) => {
-       console.log(image["id"])
         const card = <Card
         key={image["id"]}  // what is the best thing to use if you dont have an id?
-        onClick={ () => setCardsFaceUp([ ...cardsFaceUp, image["id"] ])}
-        isFaceUp = { cardsFaceUp.includes(image["id"]) || cardsMatched.includes(index) }
+        onClick={ () => setCardsFaceUp([ ...cardsFaceUp, index ])}
+        isFaceUp = { cardsFaceUp.includes(index) || cardsMatched.includes(index) }
         className={image["imgName"]}
         imgSrc = {image["img"]}
         />
@@ -30,25 +29,29 @@ function Gameboard(props){
         return card
    })
    console.log("Done with setting cards")
+   console.log(images[0].imgName)
    
-   useEffect(()=> {
-    //shuffle arrray
-    let shuffledCardArr = cardArr
-        .map((value) => ({value, sort: Math.random() }))
-        .sort((a,b) => a.sort - b.sort)
-        .map(({value}) => value)
+//    useEffect(()=> {
+//     //shuffle arrray
+//     let shuffledCardArr = cardArr
+//         .map((value) => ({value, sort: Math.random() }))
+//         .sort((a,b) => a.sort - b.sort)
+//         .map(({value}) => value)
 
-    }, [])
+//     }, [])
 
   
     useEffect(()=> {
         // console.log(cardsFaceUp)
         if (cardsFaceUp.length === 2){
-            const { idxCard1, idxCard2 } = cardsFaceUp
-            // need to get class
-            if (idxCard1 === idxCard2) {
-                setCardsMatched(idxCard1, idxCard2)
-            }
+            const [ idxCard1, idxCard2 ] = cardsFaceUp
+        
+            const card1 = images[idxCard1].imgName
+            const card2 = images[idxCard2].imgName
+        
+            if (card1 === card2) {
+                setCardsMatched([...cardsMatched, idxCard1, idxCard2])
+            } 
             setCardsFaceUp([])
         } 
 
