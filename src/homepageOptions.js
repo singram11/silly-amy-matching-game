3c9fcc64ee1b5190985d3647162e27c0b2771ea6
singram11 <ingramsarah11@gmail.com>
2021-11-22 19:17:46 -0800

    Add styles to Homepage options box

--- src/homepageOptions.js
+++ src/homepageOptions.js
@@ -1,9 +1,16 @@
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import PlayButton from "./PlayButton";
 
 function HomepageOptions() {
 
-    return <div className="mx-auto"><PlayButton/></div>
+    return <div className="homepage d-flex align-items-center justify-content-center">
+        <div className="options-card d-flex justify-content-center">
+            <Container>
+                <Row className="m-3"><p>Game Options</p></Row>
+                <Row className="mx-3"><PlayButton /></Row>
+            </Container>
+        </div>
+            </div>
 }
 
 export default HomepageOptions;
