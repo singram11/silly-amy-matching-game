b11d7433d5cb225eb706640e703c15afe2c5f3ea
singram11 <ingramsarah11@gmail.com>
2021-11-23 18:59:40 -0800

    Add dog images and dog deck file

---
+++ src/dogDeck.js
@@ -0,0 +1,23 @@
+import dog1 from "./images/dogs/dog1.jpeg"
+import dog2 from "./images/dogs/dog2.jpeg"
+import dog3 from "./images/dogs/dog3.jpeg"
+import dog4 from "./images/dogs/dog4.jpeg"
+import dog5 from "./images/dogs/dog5.jpeg"
+import dog6 from "./images/dogs/dog6.jpeg"
+
+const dogs = [
+    {"id": 1, "imgName": "dog1", "img": dog1, "alt": "winking dog" },
+    {"id": 2, "imgName": "dog2", "img": dog2, "alt": "puppy golden" },
+    {"id": 3, "imgName": "dog3", "img": dog3, "alt": "angry dog"},
+    {"id": 4, "imgName": "dog4", "img": dog4, "alt": "dog with side eye"},
+    {"id": 5, "imgName": "dog5", "img": dog5, "alt": "puppy eyed pug"},
+    {"id": 6, "imgName": "dog6", "img": dog6, "alt": "smiling pup"},
+    {"id": 7, "imgName": "dog1", "img": dog1, "alt": "winking dog" },
+    {"id": 8, "imgName": "dog2", "img": dog2, "alt": "puppy golden" },
+    {"id": 9, "imgName": "dog3", "img": dog3, "alt": "angry dog"},
+    {"id": 10, "imgName": "dog4", "img": dog4, "alt": "dog with side eye"},
+    {"id": 11, "imgName": "dog5", "img": dog5, "alt": "puppy eyed pug"},
+    {"id": 12, "imgName": "dog6", "img": dog6, "alt": "smiling pup"}
+]
+
+export default dogs
